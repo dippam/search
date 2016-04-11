6147ba2b872493e76629dbc92b067f9eb8f70f38
Conail Stewart <conailstewart@gmail.com>
2016-04-11 13:57:47 -0400

    Add an interview gender field.

--- models/eppi/Interview.go
+++ models/eppi/Interview.go
@@ -4,22 +4,26 @@ import "time"
 import "../place"
 
 type Interview struct {
-	AgeGroup     byte   `json:"ageGroup"`
-	Birthplace   Place  `json:"birthplace"`
-	Childhood    Place  `json:"childhood"`
-	Code         string `json:"code"`
-	Date         Date   `json:"date"`
-	Denomination byte   `json:"denomination"`
-	Duration     uint16 `json:"duration"`
-	Gender       byte   `json:"gender"`
-	Id           byte   `json:"id"`
-	Residence    Place  `json:"residence"`
-	Summary      string `json:"summary"`
+	Id           byte         `json:"id"`
+	AgeGroup     byte         `json:"ageGroup"`
+	Birthplace   Place        `json:"birthplace"`
+	Childhood    Place        `json:"childhood"`
+	Code         string       `json:"code"`
+	Date         Date         `json:"date"`
+	Denomination Denomination `json:"denomination"`
+	Duration     uint16       `json:"duration"`
+	Gender       Gender       `json:"gender"`
+	Residence    Place        `json:"residence"`
+	Summary      string       `json:"summary"`
 }
 
 type Interviews []Interview
 
 type Denomination struct {
 	Id   byte   `json:"id"`
+	Name string `json:"name"`
+}
+
+type Gender struct {
 	Name string `json:"name"`
 }
